1f3ad97a01ef61577866d95ff9d0889bb69c35ab
ADELINE TUYIZERE <a.tuyizere@alustudent.com>
2023-08-10 09:40:41 +0200

    all levels working

--- api.py
+++ api.py
@@ -12,12 +12,16 @@ db_connection = mysql.connector.connect(
 )
 
 # Function to save registration data to the database
+
+
 def save_registration_data(session_id, phonenumber, fullname, language, electoral_ward, national_id):
     cursor = db_connection.cursor()
     query = "INSERT INTO registrations (session_id, phonenumber, fullname, language, electoral_ward, national_id) VALUES (%s, %s, %s, %s, %s, %s)"
-    values = (session_id, phonenumber, fullname, language, electoral_ward, national_id)
+    values = (session_id, phonenumber, fullname,
+              language, electoral_ward, national_id)
     cursor.execute(query, values)
     db_connection.commit()
+
 
 @app.route("/", methods=["POST"])
 def ussd_handler():
@@ -27,7 +31,8 @@ def ussd_handler():
 
     # Retrieve the current user's language selection and registration progress from the database (if available)
     cursor = db_connection.cursor()
-    cursor.execute("SELECT language, progress FROM registrations WHERE session_id = %s", (session_id,))
+    cursor.execute(
+        "SELECT language, progress FROM registrations WHERE session_id = %s", (session_id,))
     row = cursor.fetchone()
     if row:
         language_selection, progress = row
@@ -42,8 +47,11 @@ def ussd_handler():
 
         if language_selection == "1":
             response = "CON You selected English.\nPlease enter your full name"
-        else:
+        elif language_selection == "2":
             response = "CON Wahisemo Ikinyarwanda.\nAndika amazina yawe yose"
+        else:
+            response = "END invalid input try again later!"
+
     elif progress == 1 and text:
         fullname = text
         progress = 2
@@ -59,16 +67,17 @@ def ussd_handler():
         if language_selection == "1":
             response = "CON Please enter your national ID number"
         else:
-            response = "CON Andika numero y'indangamuntu"
+            response = "CON Andika numero y'indangamuntu yawe"
     elif progress == 3 and text:
         national_id = text
-        save_registration_data(session_id, phonenumber, fullname, language_selection, district_name, national_id)
+        save_registration_data(session_id, phonenumber, fullname,
+                               language_selection, district_name, national_id)
         progress = 4
 
         if language_selection == "1":
             response = "CON Registration successful. Thank you!"
         else:
-            response = "CON kwiyandikisha byagenze neza.\nMurakoze!"
+            response = "CON Kwiyandikisha byagenze neza. Murakoze!"
     elif progress == 4 and text == "1":
         progress = 0
         response = "CON Welcome back. Please choose your language:\n1. English\n2. Kinyarwanda"
@@ -78,10 +87,13 @@ def ussd_handler():
 
     # Update progress in the database
     if progress >= 0:
-        cursor.execute("UPDATE registrations SET progress = %s WHERE session_id = %s", (progress, session_id))
+        cursor.execute(
+            "UPDATE registrations SET progress = %s WHERE session_id = %s", (progress, session_id))
         db_connection.commit()
 
     return response, 200, {'Content-type': 'text/plain'}
 
+
 if __name__ == "__main__":
+    app.debug = False
     app.run(host="localhost", port=5000)
